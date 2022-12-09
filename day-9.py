18521aff35158bccc6e6423f993f313d7c77fe4a
Knut Hühne <knut@k-nut.eu>
2022-12-09 17:50:01 +0100

    Add solution for day 9 part 2

--- day-9.py
+++ day-9.py
@@ -11,21 +11,34 @@ class Point:
 
 @dataclass
 class Field:
-    headPosition: Point = Point(0, 0)
-    tailPosition: Point = Point(0, 0)
-    tail_positions = {(0, 0)}
+    parts: list[Point]
+    tail_positions: set
     step = 0
 
+    def __init__(self, length):
+        self.parts = [Point(0, 0) for _ in range(length)]
+        self.tail_positions = {(0,0)}
+
+
+    @property
+    def headPosition(self):
+        return self.parts[0]
+
+    @property
+    def tailPosition(self):
+        return self.parts[-1]
+
     def move_head(self, direction):
+        head = self.parts[0]
         match direction:
             case 'U':
-                self.headPosition.y += 1
+                head.y += 1
             case 'D':
-                self.headPosition.y -= 1
+                head.y -= 1
             case 'L':
-                self.headPosition.x -= 1
+                head.x -= 1
             case 'R':
-                self.headPosition.x += 1
+                head.x += 1
         self.move_tail()
 
         # print(self.print())
@@ -34,37 +47,50 @@ class Field:
         self.step += 1
 
     def move_tail(self):
-        delta_x = self.headPosition.x - self.tailPosition.x
-        delta_y = self.headPosition.y - self.tailPosition.y
+        for i in range(1, len(self.parts)):
+            previous = self.parts[i-1]
+            next = self.parts[i]
 
-        if abs(delta_x) < 2 and abs(delta_y) < 2:
-            return
+            delta_x = previous.x - next.x
+            delta_y = previous.y - next.y
 
-        if abs(delta_x) > 0 and abs(delta_y) > 0:
-            # Handle a diagonal move
-            self.tailPosition.x += math.copysign(1, delta_x)
-            self.tailPosition.y += math.copysign(1, delta_y)
-        else:
-            if delta_x:
-                self.tailPosition.x += math.copysign(1, delta_x)
-            if delta_y:
-                self.tailPosition.y += math.copysign(1, delta_y)
+            if abs(delta_x) < 2 and abs(delta_y) < 2:
+                return
+
+            if abs(delta_x) > 0 and abs(delta_y) > 0:
+                # Handle a diagonal move
+                next.x += math.copysign(1, delta_x)
+                next.y += math.copysign(1, delta_y)
+            else:
+                if delta_x:
+                    next.x += math.copysign(1, delta_x)
+                if delta_y:
+                    next.y += math.copysign(1, delta_y)
 
         self.tail_positions.add((self.tailPosition.x, self.tailPosition.y))
 
     def print(self):
         rows = []
 
-        for y in range(0, 20, 1):
+        field_min = 0
+        field_max = 6
+
+        for y in range(field_min, field_max, 1):
             row = []
-            for x in range(0, 20, 1):
+            for x in range(field_min, field_max, 1):
                 if self.headPosition.x == x and self.headPosition.y == y:
                     row.append('H')
-                elif self.tailPosition.x == x and self.tailPosition.y == y:
-                    row.append('T')
-                elif (x, y) in self.tail_positions:
-                    row.append('#')
-                else:
+                    continue
+                found = False
+                for i in range(1, 10):
+                    part = self.parts[i]
+                    if part.x == x and part.y == y:
+                        found = True
+                        row.append(str(i))
+                        break
+                # if (x, y) in self.tail_positions:
+                #     row.append('#')
+                if not found:
                     row.append('.')
             rows.append(row)
         res = ""
@@ -75,19 +101,25 @@ class Field:
 
 def day_9():
     # data = get_data(9, "example")
+    # data = get_data(9, "example-2")
     data = get_data(9)
 
-    field = Field()
     moves = []
     for line in data:
         direction, steps = line.strip().split(" ")
         moves += [direction] * int(steps)
 
+    field = Field(2)
     for move in moves:
         field.move_head(move)
 
     print("Part 1: ", len(field.tail_positions))
 
+    field_2 = Field(10)
+    for move in moves:
+        field_2.move_head(move)
+
+    print("Part 2: ", len(field_2.tail_positions))
 
 if __name__ == "__main__":
     day_9()
