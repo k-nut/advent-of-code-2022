7a20986ebf2be94cad24c30b50f69c1bbe6fe48b
Knut Hühne <knut@k-nut.eu>
2022-12-11 19:38:29 +0100

    Add solution for day 11 part 2

--- day-11.py
+++ day-11.py
@@ -9,18 +9,30 @@ from helpers import get_data, split_at
 
 class Group:
     def __init__(self):
-        self.monkeys = {}
+        self._monkeys = {}
 
     def add(self, monkey):
         monkey.monkey_group = self
-        self.monkeys[monkey.number] = monkey
+        self._monkeys[monkey.number] = monkey
 
     def get(self, number):
-        return self.monkeys[number]
+        return self._monkeys[number]
 
     def run_round(self):
-        for monkey in self.monkeys.values():
+        for monkey in self.monkeys:
             monkey.inspect_and_throw()
+
+    def run_round_2(self):
+        for monkey in self.monkeys:
+            monkey.inspect_and_throw_2()
+
+    @property
+    def monkeys(self):
+        return self._monkeys.values()
+
+    @property
+    def super_divisor(self):
+        return math.prod([monkey.divider for monkey in self.monkeys])
 
 
 class Monkey:
@@ -59,6 +71,14 @@ class Monkey:
             self.monkey_group.get(target_number).add(new_worry)
         self.items = []
 
+    def inspect_and_throw_2(self):
+        for item in self.items:
+            self.inspection_count += 1
+            new_worry = self.inspect_item(item) % self.monkey_group.super_divisor
+            target_number = self.true_target if new_worry % self.divider == 0 else self.false_target
+            self.monkey_group.get(target_number).add(new_worry)
+        self.items = []
+
     def add(self, worry: int):
         self.items.append(worry)
 
@@ -74,8 +94,16 @@ def day_11():
     for _ in range(20):
         group.run_round()
 
-    *rest, second, first  = sorted(monkey.inspection_count for monkey in group.monkeys.values())
+    *rest, second, first  = sorted(monkey.inspection_count for monkey in group.monkeys)
     print("Part 1: ", first * second)
+
+    group_2 = Group()
+    for lines in split_at([l.strip() for l in data], ""):
+        group_2.add(Monkey(lines))
+    for i in range(10000):
+        group_2.run_round_2()
+    *rest, second, first  = sorted(monkey.inspection_count for monkey in group_2.monkeys)
+    print("Part 2: ", first * second)
 
 
 if __name__ == "__main__":
