407b4eb1c5cdb8053e6ce1b9d9cae747a19ebd9f
Knut Hühne <knut@k-nut.eu>
2022-12-23 12:24:33 +0100

    Add solution for day 23 part 2

--- day_23.py
+++ day_23.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from copy import copy
 
 from helpers import get_data
 
@@ -49,24 +50,27 @@ def day_23():
             if all(move(position, d) not in positions for d in direction["checks"]):
                 return position[0] + direction["move"][0], position[1] + direction["move"][1]
 
-    for i in range(10):
+    positions_10 = set()
+    for i in range(1_000_000_000):
         for elf in positions:
-            step = get_next_position(elf, i)
-            if step:
+            if step := get_next_position(elf, i):
                 proposals[step].append(elf)
 
+        if len(proposals.keys()) == 0:
+            print("Part 2:", i + 1)
+            break
         for position, candidates in proposals.items():
             if len(candidates) == 1:
                 positions.remove(candidates[0])
                 positions.add(position)
         proposals = defaultdict(list)
-        # print(f"After round {i+1}")
-        # print_field(positions)
+        if i == 9:
+            positions_10 = copy(positions)
 
-    xs = [p[0] for p in positions]
-    ys = [p[1] for p in positions]
+    xs = [p[0] for p in positions_10]
+    ys = [p[1] for p in positions_10]
 
-    print("Part 1:", (max(xs) - min(xs) + 1) * (max(ys) - min(ys) + 1) - len(positions))
+    print("Part 1:", (max(xs) - min(xs) + 1) * (max(ys) - min(ys) + 1) - len(positions_10))
 
 
 if __name__ == "__main__":
